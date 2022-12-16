07177a7da6285be2ff9334eb70d67345d7fd2e37
Max Doerner <maxdoerner@gmx.net>
2022-12-16 15:00:28 +0100

    Add preliminary version of day 16 in R
    It has the right test result, but is too slow on the real input

--- AdventOfCode2022InR/R/day16.R
+++ AdventOfCode2022InR/R/day16.R
@@ -3,20 +3,166 @@
 #' @noRd
 day16 <- modules::module(
   {
-    modules::export("parse_input")
-    parse_input <- function(input_string) {
-      stop("To Be Implemented")
+    parsing <- modules::use("R/utility/parsing_utils.R")
+    
+    parse_valve <- function(text) {
+      matches <- stringr::str_match(text, pattern = "^Valve (.+) has flow rate=(\\d+); tunnels? leads? to valves? (.+)")
+      name <- matches[[2]]
+      rate <- readr::parse_integer(matches[[3]])
+      connections <- stringr::str_split(matches[[4]], ", ") %>% unlist()
+      list(
+        name = name,
+        rate = rate,
+        connections = connections
+      )
     }
     
+    modules::export("parse_input")
+    parse_input <- function(input_string) {
+      valves <- input_string %>%
+        parsing$split_lines() %>%
+        purrr::map(parse_valve)
+      names(valves) <- purrr::map_chr(
+        .x = valves,
+        .f = function(valve) {
+          valve$name
+        }
+      )
+      valves
+    }
+    
+    encode_state <- function(state) {
+      encoded_valves <- paste0(state$open_valves, collapse = "_")
+      paste(state$position, encoded_valves, sep = "__")
+    }
+    
+    already_visited <- function(state, visited_states) {
+      encode_state(state) %in% visited_states
+    }
+    
+    available_next_actions <- function(state, valves, visited_states) {
+      current_valve <- valves[[state$position]]
+      move_states <- current_valve$connections %>%
+        purrr::map(
+          function(position) {
+            list(
+              position = position,
+              open_valves = state$open_valves
+            )
+          }
+        )
+      
+      state_already_visited <- purrr::map_lgl(
+        .x = move_states,
+        .f = purrr::partial(
+          already_visited,
+          visited_states = visited_states
+        )
+      )
+      
+      relevant_state_changes <- move_states[!state_already_visited] %>% 
+        purrr::map(
+          function(state) {
+            list(
+              state = state,
+              rate_increase = 0
+            )
+          }
+        )
+      
+      if (current_valve$rate > 0 && !(current_valve$name %in% state$open_valves)) {
+        open_valve_state <- list(
+          position = state$position,
+          open_valves = c(state$open_valves, state$position) %>%
+            unique() %>%
+            sort()
+        )
+        open_valve <- list(
+          state = open_valve_state,
+          rate_increase = current_valve$rate
+        )
+        relevant_state_changes <- list(
+          list(open_valve),
+          relevant_state_changes
+        ) %>% unlist(recursive = FALSE)
+      }
+      relevant_state_changes
+    }
+    
+    max_pressure_release_from_state <- function(
+      state, 
+      valves, 
+      remaining_time, 
+      visited_states, 
+      released_pressure, 
+      max_known_released_pressure, 
+      remaining_rate
+    ) {
+      if (remaining_time <= 0) {
+        return(max_known_released_pressure)
+      }
+      available_pressure <- remaining_time * remaining_rate
+      if (available_pressure <= max_known_released_pressure - released_pressure) {
+        # There is no more chance release more pressure then the current best.
+        return(max_known_released_pressure)
+      }
+      visited_states <- c(visited_states, encode_state(state)) 
+      new_remaining_time = remaining_time - 1L
+      
+      available_next_moves <- available_next_actions(state, valves, visited_states)
+      for (action in available_next_moves) {
+        new_released_pressure <- released_pressure + action$rate_increase * new_remaining_time
+        if (new_released_pressure > max_known_released_pressure) {
+          max_known_released_pressure <- new_released_pressure
+        }
+        max_known_released_pressure <- max_pressure_release_from_state(
+          state = action$state, 
+          valves = valves, 
+          remaining_time = new_remaining_time, 
+          visited_states = visited_states, 
+          released_pressure = new_released_pressure, 
+          max_known_released_pressure = max_known_released_pressure,
+          remaining_rate = remaining_rate - action$rate_increase 
+        )
+      }
+      max_known_released_pressure
+    }
+    
+    highest_releaseable_pressure <- function(start_point, valves, time_available) {
+      start_state <- list(
+        position = start_point,
+        open_valves = c()
+      )
+      visited_states <- c()
+      total_rate <- valves %>%
+        purrr::map_int(function(valve) valve$rate) %>%
+        sum()
+      max_pressure_release_from_state(
+        state = start_state,
+        valves = valves, 
+        remaining_time = time_available, 
+        visited_states = visited_states, 
+        released_pressure = 0, 
+        max_known_released_pressure = 0,
+        remaining_rate = total_rate
+      )
+    }
     
     modules::export("solve_part1")
-    solve_part1 <- function(input) {
-      stop("To Be Implemented")
+    solve_part1 <- function(valves) {
+      start_point <- "AA"
+      time_available <- 30L
+      max_pressure_release <- highest_releaseable_pressure(
+        start_point,
+        valves,
+        time_available
+      )
+      as.character(max_pressure_release)
     }
     
     
     modules::export("solve_part2")
-    solve_part2 <- function(input) {
+    solve_part2 <- function(valves) {
       stop("To Be Implemented")
     }
   }
@@ -39,8 +185,8 @@ parse_day16_input <- function(input_string) {
 #'
 #' @return TO DO
 #' @importFrom magrittr %>%
-solve_day16_part1 <- function(input) {
-  day16$solve_part1(input)
+solve_day16_part1 <- function(valves) {
+  day16$solve_part1(valves)
 }
 
 #' Solution to Day16 Part2 of Advent of Code 2022
@@ -49,6 +195,6 @@ solve_day16_part1 <- function(input) {
 #'
 #' @return TO DO
 #' @importFrom magrittr %>%
-solve_day16_part2 <- function(input) {
-  day16$solve_part2(input)
+solve_day16_part2 <- function(valves) {
+  day16$solve_part2(valves)
 }
--- AdventOfCode2022InR/tests/testthat/test-day16.R
+++ AdventOfCode2022InR/tests/testthat/test-day16.R
@@ -1,23 +1,185 @@
 
 
-# test_that("parse_day16_input works: provided test input", {
-#   input <- NULL
-#   actual <- parse_day16_input(input)
-#   expected <- NULL
-#   expect_equal(actual, expected)
-# })
+test_that("parse_day16_input works: provided test input", {
+  input <- "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II"
+  actual <- parse_day16_input(input)
+  expected <- list(
+    "AA" = list(
+      name = "AA",
+      rate = 0L,
+      connections = c("DD", "II", "BB")
+    ),
+    "BB" = list(
+      name = "BB",
+      rate = 13L,
+      connections = c("CC", "AA")
+    ),
+    "CC" = list(
+      name = "CC",
+      rate = 2L,
+      connections = c("DD", "BB")
+    ),
+    "DD" = list(
+      name = "DD",
+      rate = 20L,
+      connections = c("CC", "AA", "EE")
+    ),
+    "EE" = list(
+      name = "EE",
+      rate = 3L,
+      connections = c("FF", "DD")
+    ),
+    "FF" = list(
+      name = "FF",
+      rate = 0L,
+      connections = c("EE", "GG")
+    ),
+    "GG" = list(
+      name = "GG",
+      rate = 0L,
+      connections = c("FF", "HH")
+    ),
+    "HH" = list(
+      name = "HH",
+      rate = 22L,
+      connections = c("GG")
+    ),
+    "II" = list(
+      name = "II",
+      rate = 0L,
+      connections = c("AA", "JJ")
+    ),
+    "JJ" = list(
+      name = "JJ",
+      rate = 21L,
+      connections = c("II")
+    )
+  )
+  expect_equal(actual, expected)
+})
 
 
-# test_that("solve_day16_part1 works: provided test input", {
-#   input <- NULL
-#   actual <- solve_day16_part1(input)
-#   expected <- ""
-#   expect_equal(actual, expected)
-# })
+test_that("solve_day16_part1 works: provided test input", {
+  input <- list(
+    "AA" = list(
+      name = "AA",
+      rate = 0L,
+      connections = c("DD", "II", "BB")
+    ),
+    "BB" = list(
+      name = "BB",
+      rate = 13L,
+      connections = c("CC", "AA")
+    ),
+    "CC" = list(
+      name = "CC",
+      rate = 2L,
+      connections = c("DD", "BB")
+    ),
+    "DD" = list(
+      name = "DD",
+      rate = 20L,
+      connections = c("CC", "AA", "EE")
+    ),
+    "EE" = list(
+      name = "EE",
+      rate = 3L,
+      connections = c("FF", "DD")
+    ),
+    "FF" = list(
+      name = "FF",
+      rate = 0L,
+      connections = c("EE", "GG")
+    ),
+    "GG" = list(
+      name = "GG",
+      rate = 0L,
+      connections = c("FF", "HH")
+    ),
+    "HH" = list(
+      name = "HH",
+      rate = 22L,
+      connections = c("GG")
+    ),
+    "II" = list(
+      name = "II",
+      rate = 0L,
+      connections = c("AA", "JJ")
+    ),
+    "JJ" = list(
+      name = "JJ",
+      rate = 21L,
+      connections = c("II")
+    )
+  )
+  actual <- solve_day16_part1(input)
+  expected <- "1651"
+  expect_equal(actual, expected)
+})
 
 
 # test_that("solve_day16_part2 works: provided test input", {
-#   input <- NULL
+#   input <- list(
+#     "AA" = list(
+#       name = "AA",
+#       rate = 0L,
+#       connections = c("DD", "II", "BB")
+#     ),
+#     "BB" = list(
+#       name = "BB",
+#       rate = 13L,
+#       connections = c("CC", "AA")
+#     ),
+#     "CC" = list(
+#       name = "CC",
+#       rate = 2L,
+#       connections = c("DD", "BB")
+#     ),
+#     "DD" = list(
+#       name = "DD",
+#       rate = 20L,
+#       connections = c("CC", "AA", "EE")
+#     ),
+#     "EE" = list(
+#       name = "EE",
+#       rate = 3L,
+#       connections = c("FF", "DD")
+#     ),
+#     "FF" = list(
+#       name = "FF",
+#       rate = 0L,
+#       connections = c("EE", "GG")
+#     ),
+#     "GG" = list(
+#       name = "GG",
+#       rate = 0L,
+#       connections = c("FF", "HH")
+#     ),
+#     "HH" = list(
+#       name = "HH",
+#       rate = 22L,
+#       connections = c("GG")
+#     ),
+#     "II" = list(
+#       name = "II",
+#       rate = 0L,
+#       connections = c("AA", "JJ")
+#     ),
+#     "JJ" = list(
+#       name = "JJ",
+#       rate = 21L,
+#       connections = c("II")
+#     )
+#   )
 #   actual <- solve_day16_part2(input)
 #   expected <- ""
 #   expect_equal(actual, expected)
